e52ada883583921793ea8b0603f369a6a99bdf20
gordon.shao <gordon.shao@dkdtrillion.com>
2024-11-11 11:49:15 +0800

    feat update file name with route

---
+++ pages/pokemon/MoveBook.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const pokeStore = PokeStore();
+
+if (process.client) {
+  onMounted(async () => {
+    await pokeStore.getPokeMoveList();
+    console.log("fullMoveList", pokeStore.fullMoveList);
+    console.log("moveGenerationList", pokeStore.moveGenerationList);
+  });
+}
+</script>
+
+<template>
+  <TypeTagList :show_move_only="true" />
+</template>
+
+<style scoped></style>
